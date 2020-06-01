4fad7b55ce5ba161cb150193f6fc0e8aae0c4352
ahmedhabeeb37 <alnaib888@gmail.com>
2020-06-01 16:21:40 +0200

    loading the controller from the URL

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -17,11 +17,32 @@ class Core {
 
  public function __construct()
  {
-     $this->getUrl();
+     $url=$this->getUrl();
+
+
+     //look in controllers for first value
+     if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]).'.php')){
+         //if exists, Set as controller
+         $this->currentController =ucwords($url[0]);
+         //unset 0 index
+         unset($url[0]);
+
+     }
+
+     //Require the Controller
+     require_once ("../app/controllers/" . $this->currentController .'.php');
+
+     //Instantiate controller class
+     $this->currentController=new $this->currentController;
  }
 
     public  function getUrl(){
-     echo $_GET['url'];
+     if(isset($_GET['url'])) {
+         $url=rtrim($_GET['url']);
+         $url=filter_var($url,FILTER_SANITIZE_URL);
+         $url=explode('/',$url);
+         return $url;
+     }
  }
 
 }
